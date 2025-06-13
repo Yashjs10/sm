40b136283b7d4066f01fa16c4561c50de417b7ef
Yash <Yashawasthi854@bbdnitm.ac.in>
2025-06-13 09:44:49 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import openai
+import re
+from openai import OpenAI
+
+app = Flask(__name__)
+client = OpenAI(
+    base_url="https://integrate.api.nvidia.com/v1",
+    api_key="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"
+)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    cleaned = ""
+    if request.method == "POST":
+        user_input = request.form["prompt"]
+
+        completion = client.chat.completions.create(
+            model="nvidia/llama-3.1-nemotron-70b-instruct",
+            messages=[
+                {"role": "system", "content": "You are Shraddha's assistant and her friend , talk in hinglish and use emojis; your job is to always keep her happy and keep flirting, response in atmost 3 sentences"},
+                {"role": "user", "content": user_input}
+            ],
+            temperature=0.5,
+            top_p=1,
+            max_tokens=1024,
+            stream=False
+        )
+
+        message = completion.choices[0].message.content
+        cleaned = re.sub(r"[^\w\s,.'\"😊😂❤️🌟🥰👍\U0001F300-\U0001FAFF]", '', message)
+
+
+
+        print("AI Response:", cleaned)
+
+    return render_template("index.html", message=cleaned)
+
+if __name__ == "__main__":
+    app.run(debug=True)
